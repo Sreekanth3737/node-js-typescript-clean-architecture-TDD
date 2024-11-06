9d7c831fd3a0801d32d534599acf821b3351a5a5
Sreekanth3737 <jssreekanth777@gmail.com>
2024-11-06 11:54:52 +0530

    add cors middleware

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,5 +1,11 @@
 import express from 'express'
-
+import { corsMiddleware } from '../middlewares'
+import { jsonParser } from '../middlewares'
+import { contentTypeMiddleware } from '../middlewares'
 const app = express()
+
+corsMiddleware(app)
+jsonParser(app)
+contentTypeMiddleware(app)
 
 export { app }
---
+++ src/main/middlewares/cors/index.ts
@@ -0,0 +1,12 @@
+import { Express, NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+
+export const corsMiddleware = (app: Express) => {
+  app.use(cors({ origin: '*' }))
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('access-control-allow-methods', '*')
+    res.setHeader('access-control-allow-headers', '*')
+    res.setHeader('x-powered-by', '*')
+    next()
+  })
+}
